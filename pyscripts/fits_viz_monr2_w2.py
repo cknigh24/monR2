d29f8520c794111ef432299bb7dd5d6f909d8021
Collin Knight <collinknight11@gmail.com>
2021-06-04 19:04:58 -0230

    Add in fits_viz_template script and basic example calls

---
+++ pyscripts/fits_viz_monr2_w2.py
@@ -0,0 +1,12 @@
+# Fits Visualization MonR2 W2
+#+
+#-----------------------------------------------------------------------------
+# These templates will look at an input fits file and output some quick statistics 
+# and a histogram distribution to find an optimal scale to output an image  
+# highlighting relevant emission within the FOV  
+#-----------------------------------------------------------------------------
+#-
+
+from fits_viz_template import fits_viz as viz
+
+viz(filename = '../fits/monr2_w2.fits',a1=6,a2=2400,b1 =1600,b2 = 4095)
